827a860487e391df4dbe29f9325b7a04419d3830
Rafael Biriba <biribarj@gmail.com>
2012-05-29 22:07:26 -0300

    Updating nginx package from 1.2.0 to 1.3.0 and performing auto installation of passenger module

--- ubuntu_11-10_desktop/vps_builder.sh
+++ ubuntu_11-10_desktop/vps_builder.sh
@@ -70,14 +70,14 @@ cd pcre-8.30
 ./configure && make && make install
 cd ~/tmp
 
-wget http://nginx.org/download/nginx-1.2.0.tar.gz
+wget http://nginx.org/download/nginx-1.3.0.tar.gz
 wget http://www.grid.net.ru/nginx/download/nginx_upload_module-2.2.0.tar.gz
-tar xzvf nginx-1.2.0.tar.gz
+tar xzvf nginx-1.3.0.tar.gz
 tar xzvf nginx_upload_module-2.2.0.tar.gz
 
 gem install passenger
 apt-get install libcurl4-openssl-dev -y
-passenger-install-nginx-module --prefix=/opt/nginx --nginx-source-dir=/root/tmp/nginx-1.2.0 --extra-configure-flags=--add-module='/root/tmp/nginx_upload_module-2.2.0'
+passenger-install-nginx-module --auto --prefix=/opt/nginx --nginx-source-dir=/root/tmp/nginx-1.3.0 --extra-configure-flags=--add-module='/root/tmp/nginx_upload_module-2.2.0'
 #passenger-install-nginx-module --auto --auto-download
 cd ~
 rm -rf ~/tmp
